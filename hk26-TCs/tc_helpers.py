ef0b16cc65ebf49346cc068873925841d0b2e25e
laj-grant <lewisajgrant@gmail.com>
2026-05-19 09:16:56 +0000

    updated function to load in TC syclops

--- hk26-TCs/tc_helpers.py
+++ hk26-TCs/tc_helpers.py
@@ -2,7 +2,7 @@ import huracanpy
 import xarray as xr
 import numpy as np
 
-def compute_intensification_rate(tracks, var, timedelta, obs=False):
+def compute_intensification_rate(tracks, var, timedelta):
     """This function computes the 24-hr intensification rate for each track in the provided tracks object.
     Works with either wind or pressure variable
     Uses huracanpy's built in get_rate function to compute the rate of change of
@@ -11,38 +11,25 @@ def compute_intensification_rate(tracks, var, timedelta, obs=False):
     tracks: huracanpy tracks dataset
     var (str): name of variable to get rate of change for ('sfcwind_max' or 'psl_min')
     timedelta (int): data frequency in hours
-    obs (bool): False for model data, true for IBTrACS so that units are consistent between datasets/
     """
     dt = timedelta * 3600  # 'timedelta' hours in seconds
     
-    # Convert wind units to knots for easy comparison to obs, convert Pa min SLP to hPa also. 
     if var == 'sfcwind_max':
-        # units are 'm/s' in model, knots in obs
-        if obs:
-            conv_fact = 1
-        else:
-            conv_fact = 1.944
         output_var = 'wind_ir_24hr'
         final_units = 'knots/24hr'
     elif var == 'psl_min':
-        # units are 'Pa' in model, hPa in obs
-        if obs:
-            conv_fact = 1
-        else: 
-            conv_fact = 1/100
         output_var = 'pres_ir_24hr'
         final_units = 'hPa/24hr'
         
-    rate = tracks.hrcn.get_rate(var_name=var) * conv_fact
+    rate = tracks.hrcn.get_rate(var_name=var, centering='backward')
+    # units of rate are s^-1
     intens_24h = (rate.groupby(tracks.track_id).map(
             lambda x: (x.rolling(record=int(24/timedelta), min_periods=int(24/timedelta)).sum() * dt)))
     
     # Assign with proper variable name and add units attribute
     tracks = tracks.assign(**{output_var: intens_24h})
     tracks[output_var].attrs['units'] = final_units
-    print(f'24hr intensification rate calculated, units are {final_units}')
-
-    
+    print(f'24hr intensification rate calculated, units are {final_units}')    
     return tracks
 
 def load_syclops():
@@ -57,7 +44,10 @@ def load_syclops():
     tracks = {}
     for name, path in dataset_paths.items():
         model_track = huracanpy.load(track_dir + path + '/' + path + '_track_20200201-20210301_6h_tc_psl.csv')
-        tracks[name] = model_track
+        # convert units from m/s to knots when loading in for consistency with IBTrACS
+        model_track['sfcwind_max'] *= 1.944
+        model_track['psl_min'] *= 1/100
+        tracks[name] = model_track 
         print(f'{name} TC tracks loaded successfully')
         
     # also add IBTrACS data from huracanpy
@@ -69,9 +59,6 @@ def load_syclops():
     obs_tracks_clean = obs_tracks_overlap.where(obs_tracks_overlap.time.dt.time.isin([model_times]), drop=True)  
     obs_tracks_clean = obs_tracks_clean.rename_vars({'wind' : 'sfcwind_max' , 'slp' : 'psl_min'})
     tracks['IBTrACS'] = obs_tracks_clean
-
-    # rename the IBTrACS variables to be consistent with the model output - check huracanpy convention. 
-    
+    # renamed the IBTrACS variables to be consistent with the model output. 
     print("JTWC tracks loaded successfully")
-    
     return tracks
